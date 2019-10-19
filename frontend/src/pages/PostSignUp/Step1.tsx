52591ce57919e633b3c96d3d9988b702579ea4c2
Duclot William <william.duclot@gmail.com>
2019-10-19 16:46:09 +0100

    Add step 2 button

--- frontend/src/pages/PostSignUp/Step1.tsx
+++ frontend/src/pages/PostSignUp/Step1.tsx
@@ -1,14 +1,17 @@
 import React, { FunctionComponent } from 'react';
-import { ProgressBar } from '@blueprintjs/core';
 import Progress from './Progress';
+import Button from 'components/Button';
 
 const Step1: FunctionComponent = props => {
   return (
     <div>
       <Progress step={1} />
       <h2 className="page-title">Getting started</h2>
-      We just need you to fill out some details on you and what help you can provide in times of an
-      emergency. These details will also be used if you yourself become an evacuee.
+      <p>
+        We just need you to fill out some details on you and what help you can provide in times of
+        an emergency. These details will also be used if you yourself become an evacuee.
+      </p>
+      <Button className="pull-right">Let's go&nbsp;&nbsp;→</Button>
     </div>
   );
 };
