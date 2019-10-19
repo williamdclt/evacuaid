d192ad3da4ac97f7fadb5b91d29756f6c5d7bd38
robcronin <robc@theodo.co.uk>
2019-10-19 15:52:05 +0100

    add login

---
+++ frontend/src/pages/Callback/Callback.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+interface Props {
+  loginUser: (token: string) => void;
+  push: (pathName: string) => void;
+}
+
+class Callback extends React.PureComponent<Props> {
+  componentDidMount = () => {
+    const url = window.location.href;
+    const locStart = url.search('id_token') + 9;
+    const locEnd = url.search('&access_token');
+    const jwt = url.substr(locStart, locEnd);
+    this.props.loginUser(jwt);
+    this.props.push('/');
+  };
+  render = () => <div>Callback</div>;
+}
+
+export default Callback;
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -5,11 +5,13 @@ import Loader from './components/Loader/Loader';
 const Home = lazy(() => import('./pages/Home'));
 const Login = lazy(() => import('./pages/Login'));
 const Avatar = lazy(() => import('./pages/Avatar'));
+const Callback = lazy(() => import('./pages/Callback'));
 
 export const PATHS = {
   HOME: '/',
   LOGIN: '/login',
   AVATAR: '/avatar',
+  CALLBACK: '/callback',
 };
 
 const routes = () => (
@@ -18,6 +20,7 @@ const routes = () => (
       <Route exact path={PATHS.HOME} component={Home} />
       <Route path={PATHS.LOGIN} component={Login} />
       <Route path={PATHS.AVATAR} component={Avatar} />
+      <Route path={PATHS.CALLBACK} component={Callback} />
     </Switch>
   </Suspense>
 );
