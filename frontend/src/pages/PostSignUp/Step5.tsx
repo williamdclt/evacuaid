4601ba649b8748b3227f1f5041c9982c2089fdbb
Duclot William <william.duclot@gmail.com>
2019-10-19 20:25:42 +0100

    Add fifth step

---
+++ frontend/src/pages/PostSignUp/Step5.tsx
@@ -0,0 +1,52 @@
+import React, { FunctionComponent, useState, useCallback } from 'react';
+import Progress from './Progress';
+import Button from 'components/Button';
+import { RadioGroup, Radio } from '@blueprintjs/core';
+import { TEXT_MUTED } from '@blueprintjs/core/lib/esm/common/classes';
+import { Link } from 'react-router-dom';
+import { PATHS } from 'routes';
+import './PostSignUp.style.scss';
+import CheckboxGroup from './CheckboxGroup';
+import GraphicCheckbox from './GraphicCheckbox';
+
+enum NEED_RESOURCES {
+  PHARMACY = 'FIRST_AID',
+  DOCTOR = 'FOOD_WATER',
+  TRANSPORT = 'TRANSPORT',
+}
+
+const Step5: FunctionComponent = props => {
+  const [offeredResources, setOfferedResources] = useState<string[]>([]);
+
+  return (
+    <div>
+      <Progress step={5} />
+      <h2 className="page-title">What resources would I need?</h2>
+      <p className="mb-md">
+        Other than the essentials (food and shelter) is there anything that you would need as an
+        evacuee? If you do not need anything extra, then click 'Next'.
+      </p>
+      <p className={'mb-md ' + TEXT_MUTED}>Select as many as applicable.</p>
+      <form>
+        <CheckboxGroup value={offeredResources} onChange={setOfferedResources}>
+          <GraphicCheckbox label="Proximity to pharmacy" value={NEED_RESOURCES.PHARMACY} />
+          <GraphicCheckbox
+            label="Proximity to medical professionals"
+            value={NEED_RESOURCES.DOCTOR}
+          />
+          <GraphicCheckbox label="Transport" value={NEED_RESOURCES.TRANSPORT} />
+        </CheckboxGroup>
+      </form>
+      <div className="nav-buttons">
+        <Link to={PATHS.POST_SIGNUP + '/4'}>
+          <Button>←&nbsp;&nbsp;Previous</Button>
+        </Link>
+        <Link to={PATHS.POST_SIGNUP + '/6'}>
+          <Button>Next&nbsp;&nbsp;→</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Step5;
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -10,6 +10,7 @@ const PostSignUp2 = lazy(() => import('./pages/PostSignUp/Step2'));
 const PostSignUp3 = lazy(() => import('./pages/PostSignUp/Step3'));
 const PostSignUp3bis = lazy(() => import('./pages/PostSignUp/Step3bis'));
 const PostSignUp4 = lazy(() => import('./pages/PostSignUp/Step4'));
+const PostSignUp5 = lazy(() => import('./pages/PostSignUp/Step5'));
 
 export const PATHS = {
   HOME: '/',
@@ -29,6 +30,7 @@ const routes = () => (
       <Route exact path={PATHS.POST_SIGNUP + '/3'} component={PostSignUp3} />
       <Route exact path={PATHS.POST_SIGNUP + '/3/bis'} component={PostSignUp3bis} />
       <Route exact path={PATHS.POST_SIGNUP + '/4'} component={PostSignUp4} />
+      <Route exact path={PATHS.POST_SIGNUP + '/5'} component={PostSignUp5} />
     </Switch>
   </Suspense>
 );
