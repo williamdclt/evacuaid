ceb60b6200ebaf9560aba37eecf1c027268d21d1
Clément Pasteau <clementp@theodo.co.uk>
2019-10-20 11:25:11 +0100

    update getRating endpoint

--- backend/getRating.ts
+++ backend/getRating.ts
@@ -4,15 +4,29 @@ import fetch from 'node-fetch';
 
 export const main = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
-    const url = 'https://evacuaid-nasa.s3.eu-west-2.amazonaws.com/data.json';
+    const dataUrl = 'https://evacuaid-nasa.s3.eu-west-2.amazonaws.com/data.json';
 
     const data = event.queryStringParameters;
     let rating: number = 0;
-    if (!data) {
+    if (!data || !data.address) {
       return success({ rating: 0});
     }
-    const location = `${Math.round(parseFloat(data.latitude))},${Math.round(parseFloat(data.longitude))}`;
-    const response = await fetch(url);
+
+    const address = data.address;
+    const apiKey = process.env.GEOCODING_API_KEY
+    const mapUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${apiKey}`
+
+    const locationResponse = await fetch(mapUrl)
+    const locationJson = await locationResponse.json()
+    if (!locationJson || !locationJson.results) {
+      return success({ rating: 0});
+    }
+
+    const latitude = locationJson.results[0].geometry.location.lat;
+    const longitude = locationJson.results[0].geometry.location.lng;
+
+    const location = `${Math.round(parseFloat(latitude))},${Math.round(parseFloat(longitude))}`;
+    const response = await fetch(dataUrl);
     const json = await response.json();
     rating = json[location] ? json[location].score : 0;
 
