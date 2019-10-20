a32a457e473f88b2ccec7d8de2c7579ffccd5435
Gavin Williams <gavinw@theodo.co.uk>
2019-10-20 11:34:19 +0100

    Update live events

--- backendPython/fireCron.py
+++ backendPython/fireCron.py
@@ -1,12 +1,17 @@
 import csv
+import json
+import uuid
 from io import StringIO
 from urllib.request import Request, urlopen
+
+from boto3 import client, resource
+from boto3.dynamodb.conditions import Key
 
 
 def eventCreater(event, context):
     try:
         request = Request(
-            "https://nrt4.modaps.eosdis.nasa.gov/api/v2/content/archives/FIRMS/viirs/USA_contiguous_and_Hawaii/VIIRS_I_USA_contiguous_and_Hawaii_VNP14IMGTDL_NRT_2019232.txt"
+            "https://nrt4.modaps.eosdis.nasa.gov/api/v2/content/archives/FIRMS/viirs/USA_contiguous_and_Hawaii/VIIRS_I_USA_contiguous_and_Hawaii_VNP14IMGTDL_NRT_2019233.txt"
         )
         request.add_header(
             "Authorization", "Bearer E2243090-F269-11E9-99F5-4EF3207B60E0"
@@ -15,6 +20,46 @@ def eventCreater(event, context):
         csv_file = StringIO(response.read().decode("utf-8"))
         csv_reader = csv.DictReader(csv_file, skipinitialspace=True)
         for row in csv_reader:
-            print(row)
+            create_dynamo_item(dict(row))
     except Exception as err:
         print(err)
+
+
+def query_dynamo_item(latitude, longitude):
+    dynamodb = resource("dynamodb", region_name="eu-west-2")
+    table = dynamodb.Table("evacuaid-backend-dev-table")
+    responseLat = table.query(
+        IndexName="latitudeIndex", KeyConditionExpression=Key("latitude").eq(latitude)
+    )
+    responseLong = table.query(
+        IndexName="longitudeIndex",
+        KeyConditionExpression=Key("longitude").eq(longitude),
+    )
+    if len(responseLat["Items"]) > 0 and len(responseLong["Items"]) > 0:
+        return responseLat["Items"][0].get("uuid")
+    else:
+        return None
+
+
+def create_dynamo_item(fire):
+    current_event_uuid = query_dynamo_item(fire.get("latitude"), fire.get("longitude"))
+    CLIENT = client("dynamodb", region_name="eu-west-2")
+    if current_event_uuid is not None:
+        CLIENT.update_item(
+            TableName="evacuaid-backend-dev-table",
+            Key={"uuid": {"S": current_event_uuid}},
+            UpdateExpression=f"set json = :r",
+            ExpressionAttributeValues={":r": {"S": json.dumps(fire)}},
+            ReturnValues="UPDATED_NEW",
+        )
+    else:
+        CLIENT.put_item(
+            TableName="evacuaid-backend-dev-table",
+            Item={
+                "uuid": {"S": str(uuid.uuid4())},
+                "json": {"S": json.dumps(fire)},
+                "objectType": {"S": "new_fires"},
+                "latitude": {"S": fire.get("latitude")},
+                "longitude": {"S": fire.get("longitude")},
+            },
+        )
